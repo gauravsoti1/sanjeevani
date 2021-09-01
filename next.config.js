1ea91e7c8af1ba06a1318f8b39f5fccf1dc12590
Gaurav Sobti <gauravsobti1@gmail.com>
2021-09-01 17:02:40 +0530

    fixed url of image

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
     domains: [
       'sanjeevani-gallery.s3.ap-south-1.amazonaws.com',
       'localhost',
+      'sanjeevani-cms-media.s3.ap-south-1.amazonaws.com',
     ],
   },
 };
